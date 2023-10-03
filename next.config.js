4fef61409dceb0ab6589a830925ba77ec94150b5
garder500 <jeremy27.clara22@gmail.com>
2023-10-03 17:51:38 +0200

    Update config file

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    /* config options here */
+    distDir: 'build',
+    output:"export"
+  }
+   
+  export default nextConfig
