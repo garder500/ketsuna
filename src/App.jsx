a893c8e2e37f507bcea92426af03f1e530528e27
Jérémy <jeremy27.clara22@gmail.com>
2024-11-14 15:36:24 +0100

    Refactor project structure and update page title

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="flex flex-row items-center justify-center">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="w-20 h-20" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="w-20 h-20" />
-        </a>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">Ketsuna</h1>
+        <p className="text-lg">A simple and easy to use task management app</p>
       </div>
-      <h1 className="text-center">Vite + React</h1>
-      <div className="flex flex-col items-center justify-center">
-        <button onClick={() => setCount((count) => count + 1)} className="bg-blue-500 text-white p-2 rounded-md">
-          count is {count}
-        </button>
-        <p className="text-center">
-          Edit <code className="text-blue-500">src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-center">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
