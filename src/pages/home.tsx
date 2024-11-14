5f1f3e8e5d0838549da4223d1606f321ea42dc6a
Jérémy <jeremy27.clara22@gmail.com>
2024-11-14 15:50:43 +0100

    Refactor project structure and update dependencies

---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <>
+      <div class="flex flex-col items-center justify-center h-screen">
+        <h1 class="text-4xl font-bold">Ketsuna</h1>
+        <p class="text-lg">A simple and easy to use task management app</p>
+      </div>
+    </>
+  );
+}
