b2c067baad188d8f7730793801a500dd06c483af
garder500 <jeremy27.clara22@gmail.com>
2024-12-26 20:21:18 +0100

    Mise à jour de la configuration Next.js pour l'exportation et modification des dépendances

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const nextConfig: NextConfig = {
+  output: "export",
 };
 
-if(process.env.NODE_ENV === 'development') {
-  setupDevPlatform();
-}
 
 export default nextConfig;
