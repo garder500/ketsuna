f3628bef90d5a90002719ea8f992733855635013
Jérémy <jeremy27.clara22@gmail.com>
2024-12-26 17:54:03 +0100

    Enhance development setup and update dependencies
    - Added support for Cloudflare's development platform in next.config.ts.
    - Updated package.json to include new scripts for building and previewing pages.
    - Added @cloudflare/next-on-pages as a dependency.
    - Modified wrangler.toml to set project name and output directory for pages build.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const nextConfig: NextConfig = {
 };
+
+if(process.env.NODE_ENV === 'development') {
+  setupDevPlatform();
+}
 
 export default nextConfig;
